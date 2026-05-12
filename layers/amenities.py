da66ec744ad38ced557fa2e3ff6840910565587f
ShaneTien <yokchong98@gmail.com>
2026-05-12 14:27:09 +0800

    Create amenities.py

---
+++ layers/amenities.py
@@ -0,0 +1,22 @@
+import pydeck as pdk
+
+
+def build_amenity_layer(amenity_data):
+    if not amenity_data:
+        return []
+    data = [{
+        "name":      a.get("name", ""),
+        "theme":     a.get("theme", ""),
+        "latitude":  a.get("latitude"),
+        "longitude": a.get("longitude"),
+        "color":     a.get("color", [180, 180, 180, 240]),
+    } for a in amenity_data]
+    return [pdk.Layer(
+        "ScatterplotLayer",
+        data=data,
+        get_position=["longitude", "latitude"],
+        get_fill_color="color",
+        get_radius=35,
+        pickable=True,
+        opacity=0.9,
+    )]
